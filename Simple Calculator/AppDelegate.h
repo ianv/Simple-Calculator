d5324c74a27cfbd137f5e9fa6f1a22f151b9b6a9
Ian Vergara <ian.vergara@gmail.com>
2013-03-25 10:35:21 -0700

    Initial Commit

---
+++ Simple Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Simple Calculator
+//
+//  Created by Ian Vergara on 3/25/13.
+//  Copyright (c) 2013 Ian Vergara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Simple Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Simple Calculator
+//
+//  Created by Ian Vergara on 3/25/13.
+//  Copyright (c) 2013 Ian Vergara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
